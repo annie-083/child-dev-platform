4275c66fc1129e292fa3bf76433da2605fdb66a9
annie-083 <annie.kd083@gmail.com>
2026-09-14 18:40:50 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        {/* /pro และ /admin จะสร้างในขั้นตอนถัดไป (Professional Dashboard, Admin Dashboard) */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
